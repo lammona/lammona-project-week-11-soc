4ab61e4b67965f010c04fde51c1c836faa5e39da
Monika <mciskowska@gmail.com>
2025-02-20 21:36:40 +0000

    created css for app.css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,12 @@
+import styles from './Header.module.css';
 interface HeaderProps {
     title: string;
 }
 
 export function Header({ title }: HeaderProps) {
     return (
-        <header>
-            <h1>{title}</h1>
+        <header className={styles.header}>
+            <h1 className={styles.title}>{title}</h1>
         </header>
     )
 }
