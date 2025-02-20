8e89a49a42f59cca3bfc6d568f73788f7cd22d64
Monika <mciskowska@gmail.com>
2025-02-20 13:54:20 +0000

    created header and footer as a start

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './Components/Header/Header'
+import { Footer } from './components/footer/footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <div>
+      <Header/>
+      <Footer/>
+      </div>
+    
+    
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +36,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Welcome to my Project</h1>
+        </header>
+    )
+}
