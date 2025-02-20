d955da4de67ec224faa5a1a336ce3d402db02c3d
Monika <mciskowska@gmail.com>
2025-02-20 17:58:01 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Header } from './Components/Header/Header'
-import { Footer } from './Components/Footer/Footer'
+import  { Header }  from './Components/Header/Header'
+import  { Footer }  from './Components/Footer/Footer'
 
 
 function App() {
